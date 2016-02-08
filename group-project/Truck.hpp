5115383ab047e63ef745c3c53d22e1095f48af9e
AuriusMedalla <KeanuMedalla@cmail.carleton.ca>
2016-02-08 14:16:30 -0500

    Vehicle class definitions

---
+++ group-project/Truck.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class Truck/*: public Vehicle*/ {
+public:
+
+private:
+};
---
+++ group-project/Vehicle.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+//#include "Entity.h"
+
+class Vehicle/*: public Entity */{
+public:
+
+private:
+	int fuel;
+	int direction; //Degrees
+};
